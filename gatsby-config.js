d312b2ca9dceb72c409625b117218d64e9382a71
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-11-11 07:52:37 +0000

    gatsby-config: Add the offline mode plugin and pathprefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -52,4 +53,5 @@ module.exports = {
     //   },
     // },
   ],
+  pathPrefix: `/sudhanshu`,
 }
