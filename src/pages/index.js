ee4c7fcba4a7b99ebf4409d164562e6a316c3769
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2020-11-11 07:22:50 +0000

    pages: index: Add the hero and services section

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,11 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
+
 export default () => {
-  return <h2>index page</h2>
+  return <Layout>
+      <Hero />
+      <Services />
+  </Layout>
 }
 // ...GatsbyImageSharpFluid
