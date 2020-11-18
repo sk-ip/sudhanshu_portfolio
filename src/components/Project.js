62bfd8a0a6e3e6aeeb42388b26869ff9b11483c1
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2020-11-18 14:18:22 +0530

    components: project: Add checks for prop types

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,7 +6,9 @@ import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
 const Project = ({description, title, github, stack, url, image, index}) => {
 	return (
 		<article className="project">
-			<Image fluid={image.childImageSharp.fluid} className="project-img"/>
+			{
+				image && <Image fluid={image.childImageSharp.fluid} className="project-img"/>
+			}
 			<div className="project-info">
 				<span className="project-number">{index+1}.</span>
 				<h3>{title}</h3>
@@ -29,6 +31,13 @@ const Project = ({description, title, github, stack, url, image, index}) => {
 	)
 }
 
-Project.propTypes = {}
+Project.propTypes = {
+	title: PropTypes.string.isRequired,
+	github: PropTypes.string.isRequired,
+	url: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	image: PropTypes.object.isRequired,
+	stack: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
 
 export default Project
