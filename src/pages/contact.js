be1b04804601663995c62e6c4f54ab5f90b25a52
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2020-11-20 15:55:29 +0530

    pages: contact: Create contact page out of contact component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,11 @@
 import React from "react"
+import Contact from "../components/Contact"
 import Layout from "../components/Layout"
 
 const contact = () => {
-	return <Layout>
-		<section className="contact-page">
-			<article className="contact-form">
-				<h3>get in touch</h3>
-				<form action="https://formspree.io/f/xwkwqaed" method="POST">
-					<div className="form-group">
-						<input type="text" name="name" placeholder="name" className="form-control" />
-						<input type="email" name="email" placeholder="email" className="form-control" />
-						<textarea name="message" rows="5" placeholder="message" className="form-control"></textarea>
-						<button type="submit" className="submit-btn btn">submit here</button>
-					</div>
-				</form>
-			</article>
-		</section>
-	</Layout>
+    return (
+        <Layout><Contact /></Layout>
+    )
 }
 
 export default contact
