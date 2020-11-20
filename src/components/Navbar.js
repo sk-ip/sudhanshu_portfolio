4b111b2e502b5d037f92a0573dccd6015da95c2d
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2020-11-20 15:52:07 +0530

    components: navbar: remove the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react"
-import logo from "../assets/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import PageLinks from "../constants/links"
 
@@ -7,7 +6,6 @@ const Navbar = ({toggleSidebar}) => {
   return <nav className="navbar">
       <div className="nav-center">
           <div className="nav-header">
-              <img src={logo} alt="logo"/>
               <button type="button" className="toggle-btn" onClick={toggleSidebar}>
                   <FaAlignRight></FaAlignRight>
               </button>
