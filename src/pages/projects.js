5dc7167ee7e7c62e7a2b236fee5a7c80cea6f248
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2020-12-17 13:12:12 +0530

    pages: projects: Add seo to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,11 +2,13 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
+import SEO from "../components/SEO"
 
 const ProjectsPage = ({
 	data: {allStrapiProjects: {nodes: projects}}
 }) => {
 	return <Layout>
+		<SEO title="Projects" description="This is Projects page"/>
 		<section className="projects-page">
 			<Projects projects={projects} title="All Projects" />
 		</section>
