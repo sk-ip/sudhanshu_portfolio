ea7f75a0d3dab86eacd281922cd43c1b8ae992cc
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2020-12-17 13:11:12 +0530

    pages: contact: Add seo to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Contact from "../components/Contact"
 import Layout from "../components/Layout"
+import SEO from "../components/SEO"
 
 const contact = () => {
     return (
-        <Layout><Contact /></Layout>
+        <Layout>
+            <SEO title="Contact" description="This is Contact Me page"/>
+            <Contact />
+        </Layout>
     )
 }
 
