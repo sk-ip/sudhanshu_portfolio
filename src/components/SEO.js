d49998d1172ca72357a624c3d25476008c37b294
Sudhanshu Kumar <sudhanshukumar5459@gmail.com>
2020-12-17 13:08:54 +0530

    src: components: seo: Add the seo

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,8 +2,25 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const SEO = () => {
-  return <div>SEO Component</div>
+
+const query = graphql`
+{
+	site {
+		siteMetadata {
+			siteDesc: description
+			siteTitle: title
+		}
+	}
+}
+`
+
+const SEO = ({title, description}) => {
+	const {site}  = useStaticQuery(query);
+	const {siteDesc, siteTitle} = site.siteMetadata;
+
+	return <Helmet htmlAttributes={{lang: "en"}} title={`${title} | ${siteTitle}`}>
+		<meta name="description" content={description || siteDesc} />
+	</Helmet>
 }
 
 export default SEO
